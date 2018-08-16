47d372ed71793f00da8d75ce623f306ab9b7225c
NeethaC <37862946+NeethaC@users.noreply.github.com>
2018-08-16 12:41:02 +0530

    Department model class created

---
+++ Dept_model.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Neetha
+ */
+public class Dept_model {
+    int department_id;
+    String deptname;
+
+    public int getDepartment_id() {
+        return department_id;
+    }
+
+    public void setDepartment_id(int department_id) {
+        this.department_id = department_id;
+    }
+
+    public String getDeptname() {
+        return deptname;
+    }
+
+    public void setDeptname(String deptname) {
+        this.deptname = deptname;
+    }
+    
+}
