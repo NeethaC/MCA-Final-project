5cc2114369dea4d5ee60654d41ffba8d016c8511
NeethaC <37862946+NeethaC@users.noreply.github.com>
2018-08-16 12:32:34 +0530

    college model class created

---
+++ College_model.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Neetha
+ */
+public class College_model {
+    int college_id;
+    int login_id;
+    String collegename;
+    String place;
+    String pincode;
+    String email;
+    String phone;
+
+    public int getCollege_id() {
+        return college_id;
+    }
+
+    public void setCollege_id(int college_id) {
+        this.college_id = college_id;
+    }
+
+    public int getLogin_id() {
+        return login_id;
+    }
+
+    public void setLogin_id(int login_id) {
+        this.login_id = login_id;
+    }
+
+    public String getCollegename() {
+        return collegename;
+    }
+
+    public void setCollegename(String collegename) {
+        this.collegename = collegename;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    
+}
