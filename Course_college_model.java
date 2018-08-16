6929fe0bf4bf67a6fd73220d1fe31e69f04a0c84
NeethaC <37862946+NeethaC@users.noreply.github.com>
2018-08-16 12:44:11 +0530

    Course collge  model class created

---
+++ Course_college_model.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Neetha
+ */
+public class Course_college_model {
+    int id;
+    int course_id;
+    int college_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCourse_id() {
+        return course_id;
+    }
+
+    public void setCourse_id(int course_id) {
+        this.course_id = course_id;
+    }
+
+    public int getCollege_id() {
+        return college_id;
+    }
+
+    public void setCollege_id(int college_id) {
+        this.college_id = college_id;
+    }
+    
+}
