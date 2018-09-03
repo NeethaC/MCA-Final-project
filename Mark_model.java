00f2e1f01b0560ade432cacdd8c4a7401ab5d343
NeethaC <37862946+NeethaC@users.noreply.github.com>
2018-09-03 16:13:36 +0530

    mark model class created

---
+++ Mark_model.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Neetha
+ */
+public class Mark_model {
+    int mark_id;
+    int stud_id;
+    int subject_id;
+    String internal;
+    String external;
+    String status;
+
+    public int getMark_id() {
+        return mark_id;
+    }
+
+    public void setMark_id(int mark_id) {
+        this.mark_id = mark_id;
+    }
+
+    public int getStud_id() {
+        return stud_id;
+    }
+
+    public void setStud_id(int stud_id) {
+        this.stud_id = stud_id;
+    }
+
+    public int getSubject_id() {
+        return subject_id;
+    }
+
+    public void setSubject_id(int subject_id) {
+        this.subject_id = subject_id;
+    }
+
+    public String getInternal() {
+        return internal;
+    }
+
+    public void setInternal(String internal) {
+        this.internal = internal;
+    }
+
+    public String getExternal() {
+        return external;
+    }
+
+    public void setExternal(String external) {
+        this.external = external;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+}
