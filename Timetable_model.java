13de8c9b64e44a1d6098b9a3eb00acad02b0c297
NeethaC <37862946+NeethaC@users.noreply.github.com>
2018-09-03 16:14:19 +0530

    timetable model class created

---
+++ Timetable_model.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Neetha
+ */
+public class Timetable_model {
+    int timetable_id;
+    int subject_id;
+    int exam_id;
+    String date;
+    String time;
+
+    public int getTimetable_id() {
+        return timetable_id;
+    }
+
+    public void setTimetable_id(int timetable_id) {
+        this.timetable_id = timetable_id;
+    }
+
+    public int getSubject_id() {
+        return subject_id;
+    }
+
+    public void setSubject_id(int subject_id) {
+        this.subject_id = subject_id;
+    }
+
+    public int getExam_id() {
+        return exam_id;
+    }
+
+    public void setExam_id(int exam_id) {
+        this.exam_id = exam_id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+
+}
