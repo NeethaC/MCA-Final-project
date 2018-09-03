3e891f1831395564171968c127d20078a15fd14b
NeethaC <37862946+NeethaC@users.noreply.github.com>
2018-09-03 16:13:21 +0530

    exam register model class created

---
+++ Exam_register_model.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Neetha
+ */
+public class Exam_register_model {
+    int exam_register_id;
+    int login_id;
+    int timetable_id;
+
+    public int getExam_register_id() {
+        return exam_register_id;
+    }
+
+    public void setExam_register_id(int exam_register_id) {
+        this.exam_register_id = exam_register_id;
+    }
+
+    public int getLogin_id() {
+        return login_id;
+    }
+
+    public void setLogin_id(int login_id) {
+        this.login_id = login_id;
+    }
+
+    public int getTimetable_id() {
+        return timetable_id;
+    }
+
+    public void setTimetable_id(int timetable_id) {
+        this.timetable_id = timetable_id;
+    }
+    
+    
+    
+}
